62c594abd79ed7c297f7d19a8e6b5e34829dff5b
Kenfin2017 <kenfin1996@gmail.com>
2018-09-20 10:51:53 +0300

    Initialize again

---
+++ Exercise1a3/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 9/6/2018 11:13:13 AM
+ * Author : Minh
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ Exercise1a3/traffic/Sketch.cpp
@@ -0,0 +1,45 @@
+﻿/************************************************************************/
+/*    
+Name:		 Sketch.cpp
+Author:		 Minh Nguyen, Ajit Bahadur Thapa
+Description: Blinking traffic lights    
+*/
+/************************************************************************/
+
+#include <Arduino.h>
+
+#define LEDGREEN 8
+#define LEDYELLOW 9
+#define LEDRED 10
+
+void setup(){
+    pinMode(LEDGREEN, OUTPUT);
+    pinMode(LEDYELLOW, OUTPUT);
+    pinMode(LEDRED, OUTPUT);
+}
+
+void loop(){
+    //first sequence: red - delay 2 sec
+    digitalWrite(LEDRED, HIGH);
+    digitalWrite(LEDYELLOW, LOW);
+    digitalWrite(LEDGREEN, LOW);
+    delay(2000);
+
+    //second sequence: red&yellow - delay 0.5 sec
+    digitalWrite(LEDRED, HIGH);
+    digitalWrite(LEDYELLOW, HIGH);
+    digitalWrite(LEDGREEN, LOW);
+    delay(500);
+
+    //third sequence: green - delay 2 sec
+    digitalWrite(LEDRED, LOW);
+    digitalWrite(LEDYELLOW, LOW);
+    digitalWrite(LEDGREEN, HIGH);
+    delay(2000);
+
+    //fourth sequence: yellow - delay 0.5 sec
+    digitalWrite(LEDRED, LOW);
+    digitalWrite(LEDYELLOW, HIGH);
+    digitalWrite(LEDGREEN, LOW);
+    delay(500);
+}
